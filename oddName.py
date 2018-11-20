c707f6c8c275a0c63f4dccd7f033b97f9d78e1df
SeanSiowShao <seanshaoen.siow@my.jcu.edu.au>
2018-11-20 14:46:06 +0800

    complete program on oddname

---
+++ oddName.py
@@ -0,0 +1,14 @@
+def main():
+    name = get_name()
+    num=int(intput("Enter the skip value"))
+    print_name(name,num)
+
+def print_name(name,num):
+    print(name(::num)
+
+def get_name():
+    name=input("enter name")
+    while len(name)<1:
+        name = input("Enter looper names>> ")
+        return name
+main()
